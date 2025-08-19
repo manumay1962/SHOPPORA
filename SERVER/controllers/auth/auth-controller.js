2714b3ac3ba9944b054d0d6a906f8e41331fe67e
manumay@1962 <manumay8@gmail.com>
2025-08-20 01:48:51 +0530

    changing cookie config

--- SERVER/controllers/auth/auth-controller.js
+++ SERVER/controllers/auth/auth-controller.js
@@ -56,7 +56,7 @@ const loginUser = async (req, res) => {
     );
 
     res
-      .cookie("token", token, { httpOnly: true, secure: false, sameSite: 'lax' })
+      .cookie("token", token, { httpOnly: true, secure: true, sameSite: 'lax' })
       .json({
         success: true,
         message: "Logged in Succesfully",
